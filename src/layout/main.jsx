41ca1d0adf4405e2f54c8d6dbdd228f43751e48b
Keil-ENZO <enzo.keil06@icloud.com>
2023-09-19 12:21:24 +0200

    Project finished

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -1,34 +1,29 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Card from "../components/Card.jsx";
 
-
-import axios from "axios";
-import {useEffect, useState} from "react";
-import strRandom from "string-random";
 const Main = () => {
-
     const [data, setData] = useState([]);
-    const letter = strRandom(1, {
-        letters: 'abcdefghijklmnopqrstuvwxyz',
-        includeUpperCase: true,
-    });
-
+    const letter = "a"; // Vous pouvez remplacer ceci par votre génération aléatoire
 
     useEffect(() => {
-        axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
+        axios
+            .get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
             .then((res) => {
                 console.log(res.data);
                 setData(res.data);
             })
-    }, []);
+            .catch((error) => {
+                console.error("Erreur lors de la récupération des données :", error);
+                // Vous pouvez ajouter une gestion des erreurs ici
+            });
+    }, [letter]);
 
     return (
         <div className={"flex flex-wrap items-center justify-center mt-12"}>
-            {
-                data.meals?.map((meal) => {
-                    return <Card meal={meal} key={meal.idMeal}/>
-                })
-            }
-
+            {data.meals?.map((meal) => (
+                <Card meal={meal} key={meal.idMeal} />
+            ))}
         </div>
     );
 };
